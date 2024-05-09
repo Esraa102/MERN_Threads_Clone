eeab6ea20986d0d0371b579d5d0fe2517fd82690
Esraa102 <manalellban9@gmailcom>
2024-05-09 23:17:18 +0300

    create backend server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 5001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "",
+    methods: ["GET", "POST", "PUT", "DELTE"],
+    credentials: true,
+  })
+);
 
 app.listen(port, () => {
   console.log("Server Is Running on", port);
