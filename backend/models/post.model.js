f5899e07d98de0f3ef733f86f57298164dfec335
Esraa102 <manalellban9@gmailcom>
2024-05-14 09:29:58 +0300

    create post model

---
+++ backend/models/post.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    postedBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      require: true,
+    },
+    img: {
+      type: String,
+      default: "",
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    replies: [
+      {
+        userId: {
+          type: mongoose.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        userProfilePic: {
+          type: String,
+        },
+        username: {
+          type: String,
+        },
+        likes: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model("Post", postSchema);
